5ed5d7a7a02c6ec45e8a412c27885ff803f4a35f
perri99 <130299266+perri99@users.noreply.github.com>
2024-01-24 18:33:14 +0100

    Update and rename test.py to homologous _contralateral.py

--- Codes/homologous _contralateral.py
+++ Codes/homologous _contralateral.py
@@ -9,22 +9,15 @@ from tqdm import tqdm
 from matplotlib.lines import Line2D
 import csv
 
-# Funzione per calcolare la probabilità di connessione
+
 def calculate_connection_probability(category1, category2, graph):
     total_connections = 0
     total_possible_connections = 0
-
-    # Itera su tutte le coppie di neuroni tra le due categorie
     for node1 in category1:
         for node2 in category2:
-            # Controlla se c'è una connessione tra i neuroni
             if graph.has_edge(node1, node2):
                 total_connections += 1
-
-            # Aumenta il numero totale di possibili connessioni
             total_possible_connections += 1
-
-    # Calcola la probabilità di connessione
     connection_probability = total_connections / total_possible_connections
 
     return connection_probability
@@ -63,7 +56,7 @@ for graph in subgraphs:
     all_nodes_set = left_nodes.union(right_nodes)
     for u, v, data in graph.edges(data = True):
         etype = data['attribute']
-    # Identifica i nodi coinvolti nei collegamenti tra emisferi diversi
+   
     cross_hemisphere_nodes = set()
     for source, target in graph.edges():
         if source in left_nodes and target in right_nodes:
@@ -77,14 +70,14 @@ for graph in subgraphs:
             if target not in cross_hemisphere_nodes:
                 cross_hemisphere_nodes.add(target)
             
-    # Trova i nodi collegati solamente con nodi dell'altro emisfero
+    
     exclusive_cross_hemisphere_nodes = set()
     for node in cross_hemisphere_nodes:
         neighbors = set(graph.neighbors(node))
         if all(n in (left_nodes | right_nodes) - {node} for n in neighbors) and node not in exclusive_cross_hemisphere_nodes:
             exclusive_cross_hemisphere_nodes.add(node)
 
-    # Inizializza un insieme per tenere traccia dei nodi coinvolti nei collegamenti
+    
     involved_nodes = set()
 
     # Conta il numero di nodi coinvolti nei collegamenti nello stesso emisfero
@@ -103,7 +96,7 @@ for graph in subgraphs:
             if target not in involved_nodes:
                 involved_nodes.add(target)
 
-    # Calcola il numero di nodi coinvolti
+   
     contralateral_neurons_set = cross_hemisphere_nodes-cross_hemisphere_nodes.intersection(involved_nodes)
     
     bilateral_neurons_set = cross_hemisphere_nodes - contralateral_neurons_set
@@ -113,23 +106,23 @@ for graph in subgraphs:
     
     print(f'Connection: {etype}')
     ipsi_ipsi_probability = calculate_connection_probability(ipsilateral_nodes_set, ipsilateral_nodes_set, graph)
-    print(f"Probabilità di connessione tra neuroni ipsi-ipsi: {ipsi_ipsi_probability}")
+    print(f"Connection probability between ipsi-ipsi: {ipsi_ipsi_probability}")
     ipsi_bi_probability = calculate_connection_probability(ipsilateral_nodes_set, bilateral_neurons_set, graph)
-    print(f"Probabilità di connessione tra neuroni ipsi-bi: {ipsi_bi_probability}")
+    print(f"Connection probability between ipsi-bi: {ipsi_bi_probability}")
     ipsi_contra_probability = calculate_connection_probability(ipsilateral_nodes_set, contralateral_neurons_set, graph)
-    print(f"Probabilità di connessione tra neuroni ipsi-contra: {ipsi_contra_probability}")
+    print(f"Connection probability between ipsi-contra: {ipsi_contra_probability}")
     contra_contra_probability = calculate_connection_probability(contralateral_neurons_set, contralateral_neurons_set, graph)
-    print(f"Probabilità di connessione tra neuroni contra-contra: {contra_contra_probability}")
+    print(f"Connection probability betweencontra-contra: {contra_contra_probability}")
     bi_contra_probability = calculate_connection_probability(bilateral_neurons_set, contralateral_neurons_set, graph)
-    print(f"Probabilità di connessione tra neuroni bi-contra: {bi_contra_probability}")
+    print(f"Connection probability between bi-contra: {bi_contra_probability}")
     bi_bi_probability = calculate_connection_probability(bilateral_neurons_set, bilateral_neurons_set, graph)
-    print(f"Probabilità di connessione tra neuroni bi-bi: {bi_bi_probability}")
+    print(f"Connection probability between bi-bi: {bi_bi_probability}")
     
     homologous_contralateral_pairs = set()
     contralateral_pairs = set()
     homolog_count = 0
     
-    # Identifica coppie di neuroni contralaterali omologhi sinistra-destra
+    
     for u in left_nodes:
         if u in contralateral_neurons_set:
             u_homo = graph.nodes[u]['homologue']
@@ -152,11 +145,11 @@ for graph in subgraphs:
     contralateral_edges_homo = len(homologous_contralateral_pairs)
     
     
-    # Calcola la probabilità di sinaptizzazione diretta tra neuroni omologhi
+   
     total_homologous_pairs = contralateral_edges_homo
     direct_synapses_probability = homolog_count / contralateral_edges_homo
 
-    print(f"Coppie omologhe con neurone controlaterale: {total_homologous_pairs}")
-    print(f"Numero di coppie omologhe contralaterali con sinaptizzazione diretta: {homolog_count}")
-    print(f"Probabilità di sinaptizzazione diretta tra neuroni omologhi contralaterali: {direct_synapses_probability}")
+    print(f"Contralateral homologous couple: {total_homologous_pairs}")
+    print(f"Directed linked contralateral homologous couple: {homolog_count}")
+    print(f"Probability of a directed linked between contralateral homologous couple: {direct_synapses_probability}")
    
